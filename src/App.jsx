169271aa43919e6c07f301d09bb0752e823967aa
Dasari Kamali (LTIMINDTREE LIMITED) <v-dkamali@microsoft.com>
2025-03-03 18:02:53 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getPublicData, getApp2Data } from './api';
+import { getPublicData, postApp2Data } from './api';
 
 function App() {
   const [publicData, setPublicData] = useState('');
@@ -7,13 +7,23 @@ function App() {
 
   useEffect(() => {
     getPublicData().then(data => setPublicData(data));
-    getApp2Data().then(data => setApp2Data(data));
   }, []);
+
+  const handlePostRequest = async () => {
+    const requestData = { message: "Hello from React App 2!" }; // Example data
+    try {
+      const data = await postApp2Data(requestData);
+      setApp2Data(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
       <h1>Vite React App 2</h1>
       <p>Public Data: {publicData}</p>
+      <button onClick={handlePostRequest}>Send POST request to /api/app2</button>
       <p>App 2 Data: {app2Data}</p>
     </div>
   );
--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://kamspringweb-chc7arb5atc2cmg9.canadacentral-01.azurewebsites.net/api',
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
 export const getPublicData = async () => {
@@ -18,9 +18,9 @@ export const getPublicData = async () => {
   }
 };
 
-export const getApp2Data = async () => {
+export const postApp2Data = async (data) => {
   try {
-    const response = await api.get('/app2');
+    const response = await api.post('/app2', data); // POST request instead of GET
     return response.data;
   } catch (error) {
     console.error(error);
